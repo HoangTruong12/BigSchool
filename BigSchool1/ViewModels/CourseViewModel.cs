154188917a0b0bfa0a869deb8dc490d4e0f6c63f
DESKTOP-Q3H8OSD\hvant <hvantruong12@gmail.com>
2021-07-04 20:08:46 +0700

    Push Tuan 1

---
+++ BigSchool1/ViewModels/CourseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BigSchool1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool1.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required(ErrorMessage ="Vui lòng nhập Place")]
+        public string Place { get; set; }
+        [Required(ErrorMessage ="Vui lòng nhập Date")]
+        [FutureDate(ErrorMessage ="Ngày phải lớn hơn ngày hiện tại")]
+        public string Date { get; set; }
+        [Required(ErrorMessage ="Vui lòng nhập Time")]
+        [ValidTime(ErrorMessage ="Nhập đúng định dạng giờ:phút")]
+        public string Time { get; set; }
+        [Required(ErrorMessage ="Vui lòng nhập Category")]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime ()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
