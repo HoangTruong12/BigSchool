ab8774ebc441879860eb0c3dff30f7757fe901ac
DESKTOP-Q3H8OSD\hvant <hvantruong12@gmail.com>
2021-07-11 10:28:05 +0700

    Push Tuan 2

--- BigSchool1/Models/ApplicationDbContext.cs
+++ BigSchool1/Models/ApplicationDbContext.cs
@@ -5,12 +5,14 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
+
 namespace BigSchool1.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -20,6 +22,15 @@ namespace BigSchool1.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Course)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
+
         }
     }
 }
